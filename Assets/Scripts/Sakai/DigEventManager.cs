deb1679cc71680f386bb37845a47ef99a6120c49
jcjmwh80 <jcjmwh80.wadai@gmail.com>
2019-12-06 15:28:48 +0900

    発掘ポイントのランダム化プログラムの作成
    現状、ランダムになる数は決め打ち（今は5個）
    また、探索画面に入ると毎回ランダムになる

--- Assets/Scripts/Sakai/DigEventManager.cs
+++ Assets/Scripts/Sakai/DigEventManager.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class DigEventManager : MonoBehaviour
 {
+    private List<DigEvent> digEvents;
+    [SerializeField] private int eventPointCount;
     void Start()
     {
-        
+        digEvents = new List<DigEvent>(GetDigEventPoints("DigEvent"));
+        SetRandomEventPoint(eventPointCount);
     }
 
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    CheckIsActive();
+        //}
+    }
+
+    private void SetRandomEventPoint(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            int activeIndex = Random.Range(0, digEvents.Count);
+            digEvents[activeIndex].gameObject.SetActive(true);
+        }
+    }
+
+    //private void CheckIsActive()
+    //{
+    //    foreach(DigEvent digEvent in digEvents)
+    //    {
+    //        Debug.Log(digEvent.gameObject.name + " : " + digEvent.gameObject.activeSelf);
+    //    }
+    //}
+
+    private DigEvent[] GetDigEventPoints(string tag)
+    {
+        GameObject[] points = GameObject.FindGameObjectsWithTag(tag);
+        DigEvent[] events = new DigEvent[points.Length];
+        for(int i = 0; i < points.Length; i++)
+        {
+            events[i] = points[i].GetComponent<DigEvent>();
+            events[i].gameObject.SetActive(false);
+        }
+        Debug.Log("Even Point Count : " + events.Length);
+        return events;
     }
 }
