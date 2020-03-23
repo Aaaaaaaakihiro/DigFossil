9a20bbbd65352aa3f5113b4874d051e2f9982e7f
Akihiro <45870449+Akihiro@users.noreply.github.com>
2020-03-23 21:12:39 +0900

    セリフ再生UI用のUIコンポーネント作成。セリフUIマネージャ(Empty Object)の追加。セリフUIマネージャにセリフUIテスト用のscriptTest.csの作成

---
+++ Assets/MapNameHolderAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapNameHolderAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RectTransform rectTransform;
+    void Start()
+    {
+        rectTransform = this.gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
