c93ba2192b3778cd6480d4b199f7ae33a87a8110
Akihiro <45870449+Akihiro@users.noreply.github.com>
2020-03-25 21:45:14 +0900

    マップネームUIの作成

--- Assets/MapNameHolderAnimation.cs
+++ Assets/MapNameHolderAnimation.cs
@@ -2,19 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 public class MapNameHolderAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
-    RectTransform rectTransform;
+    private Animator animator;
+    private Text mapNameText;
+    private string currentSceneName;
+    [SerializeField]
+    private int indicateTime = 2000;
+    [SerializeField]
+    private int interbalTime = 10;
+
+    enum scenes : byte
+    {
+        Dig_Hirono,
+        Explore_Hirono,
+        Main_Hirono,
+        Title_Hirono,
+        Town_Hirono
+    }
+
     void Start()
     {
-        rectTransform = this.gameObject.GetComponent<RectTransform>();
+        //初期化
+        animator = this.GetComponent<Animator>();
+        mapNameText = this.transform.GetChild(0).gameObject.GetComponent<Text>();
+        currentSceneName = SceneManager.GetActiveScene().name;
+
+        //マップネームUIを非表示
+        animator.SetBool("isFadeIn", false);
+
+        Debug.Log("シーンネーム:" + currentSceneName);
+
+        //マップネームを設定、表示
+        setMapName(currentSceneName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetMouseButton(0))
+        //{
+        //    if (animator.GetBool("isFadeIn") == true)
+        //    {
+        //        animator.SetBool("isFadeIn", false);
+        //    }
+        //    else
+        //    {
+        //        animator.SetBool("isFadeIn", true);
+        //    }
+            
+        //}
     }
+
+    private async void activateMapNameUI()
+    {
+        await Task.Delay(interbalTime);
+
+        //UIをindicateTime分表示した後に非表示にする
+        Debug.Log("マップネームを表示");
+
+        animator.SetBool("isFadeIn", true);
+        deactivateMapNameUI();
+    }
+
+     private async void deactivateMapNameUI()
+    {
+        Debug.Log("マップネームを非表示");
+
+        await Task.Delay(indicateTime);
+        animator.SetBool("isFadeIn", false);
+    }
+
+    private void setMapName(string currentSceneName)
+    {
+
+        switch (currentSceneName)
+        {
+            case "Explore_Hirono":
+                mapNameText.text = "ホリホリ洞窟";
+                activateMapNameUI();
+                break;
+            case "Town_Hirono":
+                mapNameText.text = "ホリホリタウン";
+                activateMapNameUI();
+                break;
+            default:
+                mapNameText.text = "未定義";
+                break;
+        }
+    }
+
+    private IEnumerator Delay(int delayFrameCount)
+    {
+        for (int i = 0; i < delayFrameCount; i++)
+        {
+            yield return null;
+        }
+        //yield return new WaitForSeconds(delayFrameCount);
+    }
+
 }
