4653b2d2e009e88bb60f8f65ec71952b7e9446f7
Aaaaaaaakihiro <45870449+Aaaaaaaakihiro@users.noreply.github.com>
2020-04-14 17:39:31 +0900

    研究所シーンを追加

---
+++ Assets/Scripts/NPCCotroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCotroller : MonoBehaviour
+{
+    private GameObject lookAtTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAtTarget = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lookAtTarget)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(lookAtTarget.transform.position - transform.position, Vector3.up);
+
+            lookRotation.z = 0;
+            lookRotation.x = 0;
+
+            this.gameObject.transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.1f);
+        }
+    }
+}
